f8843efb09eb77f44f00c5dc3ab87fc7a21d8b26
EliButler <40725211+EliButler@users.noreply.github.com>
2021-04-19 23:44:50 -0500

    test Commit

---
+++ Final_Project/src/Class1Interface.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Collections;
+
+public interface Class1Interface 
+{
+	public String getNoun();
+	public String getAdjective();
+
+	public Collection getAllNouns();
+	public Collection getAllAdjectives();
+}
---
+++ Final_Project/src/Class2Interface.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Collections;
+
+public interface Class2Interface 
+{
+	public String getVerb();
+	public String getAdverb();
+
+	public Collection getAllVerbs();
+	public Collection getAllAdverbs();
+}
