3ab86a96e6bdb1a03ee0024ac01d68a5a3966509
EliButler <40725211+EliButler@users.noreply.github.com>
2021-05-04 12:28:48 -0500

    Almost there, need to add replacement clause in editRave

--- Final_Project/src/The_Raven.java
+++ Final_Project/src/The_Raven.java
@@ -8,17 +8,18 @@ import java.util.ArrayList;
 //adverb = adverb
 //femaleName = female name
 public class The_Raven extends MadLib{
-//    public ArrayList<String> Raven = createRaven();
+    public ArrayList<String> Raven = createRaven();
+    public ArrayList<String> madlibRaven = editRave();
 
 
     //The Raven Stanzas
     private ArrayList<String> createRaven(){
-        ArrayList<String> Raven = new ArrayList<String>();
+        ArrayList<String> myRaven = new ArrayList<String>();
 
-        String line1 = "Once upon a midnight  adj, while I ";
-        String line2 = " pondered adj and adj, ";
+        String line1 = "Once upon a midnight  adj , while I ";
+        String line2 = " pondered adj and adj , ";
         String line3 = "Over many a quaint and curious noun of forgotten noun ";
-        String line4 = "While I verbed, nearly napping suddenly there came a  noun.";
+        String line4 = "While I verbed , nearly napping suddenly there came a  noun.";
         String line5 = "As of noun gently rapping, rapping at my place door.";
         String line6 = "'Tis some noun , 'I muttered, 'tapping at my place door.";
         String line7 = "Only this, and nothing more.'";
@@ -30,64 +31,56 @@ public class The_Raven extends MadLib{
         String line13 = "named femaleName - ";
         String line14 = "Nameless here evermore";
 
-        Raven.add(line1);
-        Raven.add(line2);
-        Raven.add(line3);
-        Raven.add(line4);
-        Raven.add(line5);
-        Raven.add(line6);
-        Raven.add(line7);
-        Raven.add(line8);
-        Raven.add(line9);
-        Raven.add(line10);
-        Raven.add(line11);
-        Raven.add(line12);
-        Raven.add(line13);
-        Raven.add(line14);
+        myRaven.add(line1);
+        myRaven.add(line2);
+        myRaven.add(line3);
+        myRaven.add(line4);
+        myRaven.add(line5);
+        myRaven.add(line6);
+        myRaven.add(line7);
+        myRaven.add(line8);
+        myRaven.add(line9);
+        myRaven.add(line10);
+        myRaven.add(line11);
+        myRaven.add(line12);
+        myRaven.add(line13);
+        myRaven.add(line14);
 
+        return myRaven;
+    }
+
+    public ArrayList<String> editRave(){
+        ArrayList<String> madlibRaven = new ArrayList<String>();
+        for (String i : Raven) {
+            String a = i.replace("adj", getAdjective());
+            String b = i.replace("noun", getNoun());
+            String c = i.replace("adverb", getAdverb());
+            String d = i.replace("verb", getVerb());
+            String e = i.replace("verbing", getVerbing());
+            String f = i.replace("verbed", getVerbed());
+            madlibRaven.add(a);
+            madlibRaven.add(b);
+            madlibRaven.add(c);
+            madlibRaven.add(d);
+            madlibRaven.add(e);
+            madlibRaven.add(f);
+        }
+
+        return madlibRaven;
+    }
+
+    public ArrayList<String> getRaven(){
         return Raven;
     }
-//    private String line1 = "Once upon a midnight  adj, while I ";
-//    private String line2 = " pondered adj and adj, ";
-//    private String line3 = "Over many a quaint and curious noun of forgotten noun ";
-//    private String line4 = "While I verbed, nearly napping suddenly there came a  noun.";
-//    private String line5 = "As of noun gently rapping, rapping at my place door.";
-//    private String line6 = "'Tis some noun , 'I muttered, 'tapping at my place door.";
-//    private String line7 = "Only this, and nothing more.'";
-//    private String line8 = "Ah, distinctly I remember it was in the adj December,";
-//    private String line9 = "And each separate verbing ember wrought its noun upon the floor";
-//    private String line10 = "Eagerly I verbed the morrow;- adverb I had sought to borrow";
-//    private String line11 = "From my nouns surcease of sorrow - sorrow for the adj Lenore-";
-//    private String line12 = "For the adj and adj maiden whom the angels";
-//    private String line13 = "named femaleName - ";
-//    private String line14 = "Nameless here evermore";
-//
-//    Raven.add(line1);
-//    Raven.add(line2);
-//    Raven.add(line3);
-//    Raven.add(line4);
-//    Raven.add(line5);
-//    Raven.add(line6);
-//    Raven.add(line7);
-//    Raven.add(line8);
-//    Raven.add(line9);
-//    Raven.add(line10);
-//    Raven.add(line11);
-//    Raven.add(line12);
-//    Raven.add(line13);
-//    Raven.add(line14);
 
-
-
-//    public ArrayList<String> editRave(){
-//        Raven.get(0).replace("noun", getNoun());
-//
-//        return Raven;
-//    }
+    public ArrayList<String> getMadlibRaven(){
+        return madlibRaven;
+    }
 
     public static void main(String[] args) {
         The_Raven a = new The_Raven();
-        System.out.println(a.createRaven());
+        System.out.println(a.getRaven());
+        System.out.println(a.madlibRaven);
     }
 
 
